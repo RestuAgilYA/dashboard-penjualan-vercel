2aec9e89ac215391bff91d732fae587077a7cf00
RestuAgilYA <restuagil.ya@gmail.com>
2025-04-21 15:55:00 +0700

    initial commit

--- api/index.php
+++ api/index.php
@@ -1,3 +1,4 @@
 <?php
 
+// Path to Laravel bootstrap
 require __DIR__ . '/../public/index.php';
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\PenjualanController;
 
 /*
@@ -15,3 +16,9 @@ use App\Http\Controllers\PenjualanController;
 */
 
 Route::get('/', [PenjualanController::class, 'index'])->name('dashboard');
+
+// Route opsional untuk seeding database (hapus setelah digunakan di Vercel)
+Route::get('/seed', function () {
+    Artisan::call('db:seed', ['--class' => 'PenjualanSeeder']);
+    return 'Seeding done!';
+});
